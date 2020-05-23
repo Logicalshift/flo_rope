4da056560d9d06d3e08cb87b64f029cc41133bda
Andrew Hunter <andrew@logicalshift.co.uk>
2020-05-23 16:54:40 +0100

    The 'delete' action can be performed by a replace with a 0-length vec

--- src/api/rope_action.rs
+++ src/api/rope_action.rs
@@ -5,9 +5,6 @@ use std::ops::{Range};
 ///
 #[derive(Clone, PartialEq, Debug)]
 pub enum RopeAction<Cell, Attribute> {
-    /// Removes a range of text from this rope
-    Delete(Range<usize>),
-
     /// Replaces a range of text in this rope
     Replace(Range<usize>, Vec<Cell>),
 
