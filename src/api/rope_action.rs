9c80ed5d6411c74f4d73c6778e57ab4e6ce9001d
Andrew Hunter <andrew@logicalshift.co.uk>
2020-05-23 14:10:22 +0100

    Should be able to set the cell values and the attributes at the same time

--- src/api/rope_action.rs
+++ src/api/rope_action.rs
@@ -12,5 +12,8 @@ pub enum RopeAction<Cell, Attribute> {
     Replace(Range<usize>, Vec<Cell>),
 
     /// Sets the attributes for a range of text in this rope
-    SetAttributes(Range<usize>, Attribute)
+    SetAttributes(Range<usize>, Attribute),
+
+    /// Sets both the attributes and the value for a range of cells
+    ReplaceAttributes(Range<usize>, Vec<Cell>, Attribute)
 }
