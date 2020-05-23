5e326fe091c1c4d6d9c5469fb9dbcb719017f2db
Andrew Hunter <andrew@logicalshift.co.uk>
2020-05-23 20:07:35 +0100

    Add an implementation of the 'node split' routine

--- src/rope/attributed_rope.rs
+++ src/rope/attributed_rope.rs
@@ -17,8 +17,8 @@ pub struct AttributedRope<Cell, Attribute> {
     /// The index of the root node
     root_node_idx: RopeNodeIndex,
 
-    /// The number of empty nodes in the nodes list
-    free_nodes: usize
+    /// List of nodes that are not being used
+    free_nodes: Vec<usize>
 }
 
 impl<Cell, Attribute> AttributedRope<Cell, Attribute> 
@@ -32,7 +32,24 @@ where
         AttributedRope {
             nodes:          vec![RopeNode::Leaf(None, Arc::new(vec![]), Arc::new(Attribute::default()))],
             root_node_idx:  RopeNodeIndex(0),
-            free_nodes:     0
+            free_nodes:     vec![]
+        }
+    }
+
+    ///
+    /// Allocates space for a new node, stores it and returns the index that it was written to
+    ///
+    fn store_new_node(&mut self, node: RopeNode<Cell, Attribute>) -> RopeNodeIndex {
+        // Try to use an existing empty node if there is one
+        if let Some(free_node) = self.free_nodes.pop() {
+            // Store in this free node
+            self.nodes[free_node] = node;
+            RopeNodeIndex(free_node)
+        } else {
+            // Create a new node
+            let free_node = self.nodes.len();
+            self.nodes.push(node);
+            RopeNodeIndex(free_node)
         }
     }
 
@@ -41,6 +58,35 @@ where
     ///
     fn root_node<'a>(&'a self) -> &'a RopeNode<Cell, Attribute> {
         &self.nodes[self.root_node_idx.idx()]
+    }
+
+    ///
+    /// Divides a node into two (replacing a leaf node with a branch node)
+    ///
+    fn split(&mut self, leaf_node: RopeNodeIndex, index: usize) {
+        if let RopeNode::Leaf(parent, cells, attribute) = &self.nodes[leaf_node.idx()] {
+            // We split the node by cloning
+            let (parent, cells, attribute) = (parent.clone(), cells.clone(), attribute.clone());
+
+            // Split the cells into two halves
+            let left_cells      = cells[0..index].iter().cloned().collect::<Vec<_>>();
+            let right_cells     = cells[index..cells.len()].iter().cloned().collect::<Vec<_>>();
+
+            // Generate the left and right nodes (the current leaf node will become the branch node)
+            let left_node       = RopeNode::Leaf(Some(leaf_node), Arc::new(left_cells), attribute.clone());
+            let right_node      = RopeNode::Leaf(Some(leaf_node), Arc::new(right_cells), attribute.clone());
+
+            let left_idx        = self.store_new_node(left_node);
+            let right_idx       = self.store_new_node(right_node);
+
+            // Replace the leaf node with the new node
+            self.nodes[leaf_node.idx()] = RopeNode::Branch(Arc::new(RopeBranch {
+                left:   left_idx,
+                right:  right_idx,
+                length: cells.len(),
+                parent: parent
+            }));
+        }
     }
 }
 
--- src/rope/branch.rs
+++ src/rope/branch.rs
@@ -6,14 +6,14 @@ use super::node::*;
 #[derive(Clone, Debug, PartialEq)]
 pub struct RopeBranch {
     /// The left-hand side of the rope (first part of the string)
-    pub left: usize,
+    pub left: RopeNodeIndex,
 
     /// The right-hand side of the rope
-    pub right: usize,
+    pub right: RopeNodeIndex,
 
     /// The total length of all the substrings under this branch
     pub length: usize,
 
     /// The parent of this branch, or None if this is the root node
-    pub parent: RopeNodeIndex
+    pub parent: Option<RopeNodeIndex>
 }
