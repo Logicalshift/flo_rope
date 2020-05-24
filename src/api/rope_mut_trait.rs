a903086d3772937495c6e91a7442d2bb313e92c0
Andrew Hunter <andrew@logicalshift.co.uk>
2020-05-24 12:18:12 +0100

    Add function-call equivalents of the edit actions (and make the edit routine the one new implementations need to build)

--- src/api/rope_mut_trait.rs
+++ src/api/rope_mut_trait.rs
@@ -1,6 +1,8 @@
 use super::rope_trait::*;
 use super::rope_action::*;
 
+use std::ops::{Range};
+
 ///
 /// A rope that can be edited by the user
 ///
@@ -9,4 +11,26 @@ pub trait RopeMut : Rope {
     /// Performs the specified editing action to this rope
     ///
     fn edit(&mut self, action: RopeAction<Self::Cell, Self::Attribute>);
+
+    ///
+    /// Replaces a range of cells. The attributes applied to the new cells will be the same
+    /// as the attributes that were applied to the first cell in the replacement range
+    ///
+    fn replace<NewCells: IntoIterator<Item=Self::Cell>>(&mut self, range: Range<usize>, new_cells: NewCells) {
+        self.edit(RopeAction::Replace(range, new_cells.into_iter().collect()));
+    }
+
+    ///
+    /// Sets the attributes for a range of cells
+    ///
+    fn set_attributes(&mut self, range: Range<usize>, new_attributes: Self::Attribute) {
+        self.edit(RopeAction::SetAttributes(range, new_attributes));
+    }
+
+    ///
+    /// Replaces a range of cells and sets the attributes for them.
+    ///
+    fn replace_attributes<NewCells: IntoIterator<Item=Self::Cell>>(&mut self, range: Range<usize>, new_cells: NewCells, new_attributes: Self::Attribute) {
+        self.edit(RopeAction::ReplaceAttributes(range, new_cells.into_iter().collect(), new_attributes));
+    }
 }
