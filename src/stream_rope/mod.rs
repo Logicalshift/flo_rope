e061bffdd75650802e8830a52663688cf53afb13
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-27 15:16:49 +0000

    Add module documentation

--- src/stream_rope/mod.rs
+++ src/stream_rope/mod.rs
@@ -1,3 +1,12 @@
+//!
+//! # Stream rope functions
+//!
+//! The functions in this module provide support functionality for streaming changes to
+//! a rope to other parts of the application. They do not directly support the futures
+//! stream (see the rope bindings in `flo_binding` for that functionality) but rather
+//! provide the building blocks for implementing that functionality elsewhere.
+//!
+
 mod push_rope;
 mod pull_rope;
 #[cfg(test)] mod tests;
